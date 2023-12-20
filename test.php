bf8baf8ec07bbf5b8e901dc696e3e97561d772a3
amitkowm <154422312+amitkowm@users.noreply.github.com>
2023-12-20 15:40:26 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+// $new = 'https://od.upgrade.macraesdev.com/vacation-rentals/mexico/quintana-roo/canc%C3%BAn';
+// echo $new.'<br/>';
+// echo utf8_encode($new).'<br/>';
+
+$test = "https://www.ownerdirect.com/pet-friendly-vacation-rentals/anglesey";
+$exTest = explode('/',$test);
+echo '<pre>';
+print_r($exTest);
+
+die;
